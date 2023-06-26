7c896153db7546ec1cb724cae031e23c2f126a9e
HssPunya93 <137598019+HssPunya93@users.noreply.github.com>
2023-06-26 17:20:42 +0800

    Update cek-vless.sh

--- xray/cek-vless.sh
+++ xray/cek-vless.sh
@@ -52,5 +52,5 @@ oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
 echo "----------------------------------------"
-echo "Script Mod By Tarap-Kuhing"
+echo "Script Mod By Papada'an-Store"
 rm -rf /tmp/other.txt
