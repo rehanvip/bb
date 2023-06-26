7e16f3769f23de607507a5845e54e5ddb0d21600
HssPunya93 <137598019+HssPunya93@users.noreply.github.com>
2023-06-26 13:46:43 +0800

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -16,7 +16,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 
 apt install rclone -y
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/HssPunya93/hss/main/backup/rclone.conf"
 #Install Wondershaper
 cd /bin
 git clone  https://github.com/magnific0/wondershaper.git
@@ -43,11 +43,11 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/strt.sh"
-wget -O limitspeed "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/limitspeed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/HssPunya93/hss/main/backup/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/HssPunya93/hss/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/HssPunya93/hss/main/backup/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/HssPunya93/hss/main/backup/strt.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/HssPunya93/hss/main/backup/limitspeed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x restore
