3b59cc24be41e4b7aa18c4603c833385fe62a364
HssPunya93 <137598019+HssPunya93@users.noreply.github.com>
2023-06-28 00:29:52 +0800

    Update m-ip.sh

--- menu/m-ip.sh
+++ menu/m-ip.sh
@@ -603,7 +603,7 @@ echo -e ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 m-ip
 }
-Isadmin=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | grep $MYIP | awk '{print $5}')
+Isadmin=$(curl -sS https://raw.githubusercontent.com/HssPunya93/hss/main/ipvps | grep $MYIP | awk '{print $5}')
 if [ "$Isadmin" = "OFF" ]; then
 clear
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
