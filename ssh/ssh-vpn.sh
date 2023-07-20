2543484e2ba159aaa4ceaa89b5a0e665fc2b9651
rehanvip <134266179+rehanvip@users.noreply.github.com>
2023-07-20 19:50:46 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -277,8 +277,8 @@ netfilter-persistent reload
 # download script
 cd /usr/bin
 # menu
-wget -O menu-update "https://raw.githubusercontent.com/rehanvip/bb/main/menu/menu-update.sh"
-wget -O update "https://raw.githubusercontent.com/rehanvip/bb/main/menu/update.sh"
+wget -O menu-update "https://raw.githubusercontent.com/rehanvip/update/main/menu/menu-update.sh"
+wget -O update "https://raw.githubusercontent.com/rehanvip/update/main/menu/update.sh"
 wget -O menu "https://raw.githubusercontent.com/rehanvip/bb/main/menu/menu.sh"
 wget -O m-bot "https://raw.githubusercontent.com/rehanvip/bb/main/menu/m-bot.sh"
 wget -O m-ip "https://raw.githubusercontent.com/rehanvip/bb/main/menu/m-ip.sh"
