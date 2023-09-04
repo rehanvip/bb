6c190a81662012d740c77f6dc03e404917150e99
Rehan Vi <134266179+rehanvip@users.noreply.github.com>
2023-09-04 20:21:55 +0800

    Add files via upload

---
+++ backup/set-br.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#JULAK BANTUR
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+# Link Hosting Kalian
+
+apt install rclone -y
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/HssPunya93/julakhss/main/JB2/rclone.conf"
+#Install Wondershaper
+cd /bin
+git clone  https://github.com/magnific0/wondershaper.git
+cd wondershaper
+sudo make install
+cd
+rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default
+host smtp.gmail.com
+port 587
+auth on
+user backupsmtp93@gmail.com
+from backupsmtp93@gmail.com
+password sdallofkbpuhbtoa 
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+wget -O autobackup "https://raw.githubusercontent.com/HssPunya93/julakhss/main/JB2/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/HssPunya93/julakhss/main/JB2/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/HssPunya93/julakhss/main/JB2/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/HssPunya93/julakhss/main/JB2/strt.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/HssPunya93/julakhss/main/JB2/limitspeed.sh"
+wget -O xp "https://raw.githubusercontent.com/ppnhss/sc3/main/ssh/xp.sh"
+
+chmod +x autobackup
+chmod +x backup
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
+chmod +x xp
+cd
+if [ ! -f "/etc/cron.d/cleaner" ]; then
+cat> /etc/cron.d/cleaner << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/2 * * * * root /usr/bin/cleaner
+END
+fi
+
+if [ ! -f "/etc/cron.d/re_otm" ]; then
+cat> /etc/cron.d/re_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 2 * * * root /sbin/reboot
+END
+fi
+
+if [ ! -f "/etc/cron.d/xp_otm" ]; then
+cat> /etc/cron.d/xp_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * root /usr/bin/xp
+END
+fi
+cat > /home/re_otm <<-END
+7
+END
+
+if [ ! -f "/etc/cron.d/bckp_otm" ]; then
+cat> /etc/cron.d/bckp_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 5 * * * root /usr/bin/bottelegram
+END
+fi
+
+service cron restart > /dev/null 2>&1
+service cron reload >/dev/null 2>&1
+rm -f /root/set-br.sh
+
+echo ""
