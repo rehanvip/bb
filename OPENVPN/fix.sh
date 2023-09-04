7ce6c0317a634ddce0d84d9e60c59a6d35e5a065
Rehan Vi <134266179+rehanvip@users.noreply.github.com>
2023-09-04 20:12:07 +0800

    Update fix.sh

--- OPENVPN/fix.sh
+++ OPENVPN/fix.sh
@@ -9,13 +9,13 @@ rm -rf /etc/systemd/system/edu-proxy.service
 rm -rf /etc/systemd/system/ws-dropbear.service
 rm -rf /etc/systemd/system/edu-proxyovpn
 
-curl "https://raw.githubusercontent.com/bagusid93/bantur/main/julak/insshws.sh" | bash
+curl "https://raw.githubusercontent.com/HssPunya93/jb/main/julak/insshws.sh" | bash
 
 
 
-wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/bagusid93/bantur/main/julak/openssh.py && chmod +x /usr/local/bin/edu-proxy
+wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/HssPunya93/jb/main/julak/openssh.py && chmod +x /usr/local/bin/edu-proxy
 
-wget -O /etc/systemd/system/edu-proxy.service https://raw.githubusercontent.com/bagusid93/bantur/main/julak/http.service && chmod +x /etc/systemd/system/edu-proxy.service
+wget -O /etc/systemd/system/edu-proxy.service https://raw.githubusercontent.com/HssPunya93/jb/main/julak/http.service && chmod +x /etc/systemd/system/edu-proxy.service
 
 systemctl daemon-reload
 systemctl enable edu-proxy.service
