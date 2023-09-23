06537b85d37b1deec72c1f044dce6ed8c40a5239
Rehan Vi <134266179+rehanvip@users.noreply.github.com>
2023-09-24 03:02:26 +0800

    Add files via upload

---
+++ ssh/user-lock.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/julak/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m"
+COLOR1="$(cat /etc/julak/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/julak/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
+
+clear
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 ${NC} ${COLBG1}                 ${WH}• MEMBER SSH •                ${NC}$COLOR1$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo "USERNAME          EXP DATE          STATUS"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+while read expired
+do
+AKUN="$(echo $expired | cut -d: -f1)"
+ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+status="$(passwd -S $AKUN | awk '{print $2}' )"
+if [[ $ID -ge 1000 ]]; then
+if [[ "$status" = "L" ]]; then
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED"
+else
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED"
+fi
+fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo "Account number: $JUMLAH user"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1${NC} ${COLBG1}              ${WH}• LOCK USER SSH •                 ${NC}$COLOR1$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo " "
+read -p "Input Username to lock 👉 " username
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+# proses mengganti passwordnya
+passwd -l $username
+clear
+  echo " "
+  echo " "
+  echo " "
+  echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+  echo -e "Username ${blue}$username${NC} successfully ${red}LOCKED!${NC}."
+  echo -e "Access Login to username ${blue}$username${NC} has been locked."
+  echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+else
+echo "Username not found on your server."
+    exit 1
+fi
+echo -e ""
+echo -e "$COLOR1 $NC ${BICyan}[${WH}01${BICyan}]${NC} ${WH}• ${COLOR1}Back To Menu ${NC}     $COLOR1 $NC"
+echo -e "$COLOR1 $NC ${BICyan}[${WH}02${BICyan}]${NC} ${WH}• ${COLOR1}Exit Script  ${NC}     $COLOR1 $NC"
+echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
+echo -e ""
+case $opt in
+1) clear ; menu ; exit ;;
+2) exit ;;
+*) echo -e "" ; echo "salah tekan sayang" ; sleep 1 ; user-unlock ;;
+esac
